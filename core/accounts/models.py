c93a219ed1af9e974a36e597eab536acbfeffa7f
farzan alaei <farzan.70@gmail.com>
2025-02-06 00:34:27 +0330

    adding signals for create profile after user instance create

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -79,3 +81,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
