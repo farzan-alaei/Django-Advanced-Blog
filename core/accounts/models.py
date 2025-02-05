f3541ced170007b6338c979e1aeaec10bad2eb20
farzan alaei <farzan.70@gmail.com>
2025-02-05 23:11:53 +0330

    add User class to accounts app

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
