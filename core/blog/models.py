bcbb7ef9f18d08a1a6f87c8f120f0c93137cbbab
farzan alaei <farzan.70@gmail.com>
2025-02-06 00:52:56 +0330

    add user model to profile class

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+# getting user model object
+User = get_user_model()
+
+
 class Post(models.Model):
     """
     this is a class to define post for blog
