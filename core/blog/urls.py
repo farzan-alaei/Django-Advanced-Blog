b8b1a8b14b17f97f03784fd2c462cf6cede4bae5
farzan alaei <farzan.70@gmail.com>
2025-02-08 13:45:03 +0330

    add RedirectView to urls.py

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
+app_name = "blog"
 
 urlpatterns = [
     path("fbv-index/", views.indexView, name="fbv-index"),
     # path("cbv-index/",TemplateView.as_view(template_name="index.html", extra_context={"title": "cbv-index"}), name="cbv-about"),
     path("cbv-index/", views.IndexView.as_view(), name="cbv-index"),
+    path("go-to/", RedirectView.as_view(pattern_name="blog:fbv-index"), name="go-to"),
 ]
