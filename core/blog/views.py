4139134196892dee2458e98d3455961942138a67
farzan alaei <farzan.70@gmail.com>
2025-02-08 10:41:51 +0330

    adding class base view, templateview
    add template view to blog app

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
 
 # Create your views here.
+
+
+def indexView(request):
+    context = {
+        "title": "fbv-index",
+    }
+    return render(request, "index.html", context=context)
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "cbv-index"
+        context["posts"] = Post.objects.all()
+        return context
