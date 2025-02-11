84c85f710ed4bd0f42d2e4ce7d04f3d0d2f41f1d
farzan alaei <farzan.70@gmail.com>
2025-02-12 01:46:27 +0330

    adding sample permissions

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,12 +1,14 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
 from blog.models import Post
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAdminUser])
 def postList(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=True)
