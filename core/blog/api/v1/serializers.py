b84f697eb7314d294edb348b0db4ad214053aaff
farzan alaei <farzan.70@gmail.com>
2025-02-12 00:10:45 +0330

    change PostSerializer
    serlializer to ModelSerializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
+from blog.models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    status = serializers.BooleanField()
-    category = serializers.CharField()
-    created_date = serializers.DateTimeField()
-    updated_date = serializers.DateTimeField()
-    published_date = serializers.DateTimeField()
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+#     status = serializers.BooleanField()
+#     category = serializers.CharField()
+#     created_date = serializers.DateTimeField()
+#     updated_date = serializers.DateTimeField()
+#     published_date = serializers.DateTimeField()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content", "status", "created_date", "published_date"]
