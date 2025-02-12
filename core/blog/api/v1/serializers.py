cdf25cc8815bdb334fd825b29d936a334d03f4e6
farzan alaei <farzan.70@gmail.com>
2025-02-12 22:46:43 +0330

    add categoryserializer to postserializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -12,10 +12,20 @@ from blog.models import Post, Category
 #     published_date = serializers.DateTimeField()
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "name",
+        ]
+
+
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url", read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    category = CategorySerializer()
 
     class Meta:
         model = Post
@@ -37,11 +47,5 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.get_absolute_api_url())
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = [
-            "id",
-            "name",
-        ]
+    def to_representation(self, instance):
+        return super().to_representation(instance)
