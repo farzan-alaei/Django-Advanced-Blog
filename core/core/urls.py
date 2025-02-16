3eb8ed012b51af88fff993ad5e404a5822cfbc59
farzan alaei <farzan.70@gmail.com>
2025-02-16 12:20:28 +0330

    change url of documentation

--- core/core/urls.py
+++ core/core/urls.py
@@ -42,9 +42,9 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("blog/", include("blog.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    # path(
-    #     "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    # ),
+    path(
+        "swagger/api.json", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
