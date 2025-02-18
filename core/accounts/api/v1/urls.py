2274ebcdc4b57f2ccc10a2e67baa7049fe4f6594
farzan alaei <farzan.70@gmail.com>
2025-02-18 11:33:33 +0330

    customizing token authentication
    Overwrite serializer and views from djangorestframework token authentication

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from rest_framework.authtoken.views import ObtainAuthToken
+
+# from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
 
 
@@ -8,5 +9,5 @@ app_name = "api-v1"
 urlpatterns = [
     # registration
     path("registration/", views.RegistrationApiView.as_view(), name="registration"),
-    path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
+    path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
 ]
