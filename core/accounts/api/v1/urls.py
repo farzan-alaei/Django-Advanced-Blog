82fda75131d77cfa92bc9aab7074776a53ea81a5
farzan alaei <farzan.70@gmail.com>
2025-02-18 11:41:18 +0330

    delete token after logout

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # registration
     path("registration/", views.RegistrationApiView.as_view(), name="registration"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
 ]
