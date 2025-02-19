dde5699246898463c6ea4bcdae10fa26e739f7cd
farzan alaei <farzan.70@gmail.com>
2025-02-19 22:18:32 +0330

    seprating all models and urls from accounts app
    seprate models to folder
    seprate urls in api

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,12 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
-from . import views
+from .. import views
 
-
-app_name = "api-v1"
 
 urlpatterns = [
     # registration
@@ -24,6 +22,4 @@ urlpatterns = [
     path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
-    # profile
-    path("profile/", views.ProfileApiView.as_view(), name="profile"),
 ]
