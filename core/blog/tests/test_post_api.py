3986866884f7b3fd5ca4642272f5bc3a5495873d
farzan alaei <farzan.70@gmail.com>
2025-03-06 12:10:57 +0330

    change test post
    change test post for getting post with response 200

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,6 +1,11 @@
 import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-
+@pytest.mark.django_db
 class TestPostAPI:
-    def test_get_post_response_200(self):
-        assert 1 == 1
+    def test_get_post_response_200_status(self):
+        client = APIClient()
+        url = reverse("blog:api-v1:post-list")
+        response = client.get(url)
+        assert response.status_code == 200
