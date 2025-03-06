1f61b1a8627906df1c2325c3e98ad4df3f663e69
farzan alaei <farzan.70@gmail.com>
2025-03-06 12:16:48 +0330

    add test method for create post with 401 status

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,11 +1,25 @@
 import pytest
+from datetime import datetime
 from django.urls import reverse
 from rest_framework.test import APIClient
 
+
 @pytest.mark.django_db
 class TestPostAPI:
+    client = APIClient()
+
     def test_get_post_response_200_status(self):
-        client = APIClient()
         url = reverse("blog:api-v1:post-list")
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200
+
+    def test_create_post_response_401_status(self):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "test",
+            "content": "test",
+            "status": True,
+            "published_date": datetime.now(),
+        }
+        response = self.client.post(url, data)
+        assert response.status_code == 401
