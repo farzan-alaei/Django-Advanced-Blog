9215e068338c3326996850c06b64089b1fcfeebe
farzan alaei <farzan.70@gmail.com>
2025-03-17 11:30:48 +0330

    add cache decorator to views

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from .tasks import sendEmail
-from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 import requests
 
 
@@ -8,11 +8,9 @@ def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Email sent</h1>")
 
-
+@cache_page(60)
 def test(request):
-    if cache.get("test_delay_api") is None:
-        response = requests.get(
-            "https://fbe7e2ba-0dd7-498f-8c09-da15de86097d.mock.pstmn.io/test/delay/5"
-        )
-        cache.set("test_delay_api", response.json())
-    return JsonResponse(cache.get("test_delay_api"))
+    response = requests.get(
+        "https://fbe7e2ba-0dd7-498f-8c09-da15de86097d.mock.pstmn.io/test/delay/5"
+    )
+    return JsonResponse(response.json())
