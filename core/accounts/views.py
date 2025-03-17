64624ee94e95f3525cdc742fadc89c1a61ecaf5f
farzan alaei <farzan.70@gmail.com>
2025-03-17 11:04:43 +0330

    adding test response from mock server
    creating endpoint for response from mock server (postman)

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .tasks import sendEmail
+import requests
 
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Email sent</h1>")
+
+
+def test(request):
+    response = requests.get(
+        "https://fbe7e2ba-0dd7-498f-8c09-da15de86097d.mock.pstmn.io/test/delay/5"
+    )
+    return JsonResponse(response.json())
